af001aaba4d424856018fec32c934c93174cf7df
murong <allisonmrmr@outlook.com>
2024-07-29 14:45:01 +0200

    Remove unnecessary metrics calc

--- utils/compute_metrics.py
+++ utils/compute_metrics.py
@@ -234,7 +234,7 @@ def compute_metrics_other(input_dir, output_folder, path_avg_organ_volume, split
                                                pred_img=pred_binary,
                                                # csv_file='cvsfile.csv',
                                                spacing=spacing,
-                                               metrics=None)
+                                               metrics=['dice', 'hd', 'hd95'])
 
                     hd95matrix[i, key] = metrics[0]['hd95'][0]
                     hdmatrix[i, key] = metrics[0]['hd'][0]
