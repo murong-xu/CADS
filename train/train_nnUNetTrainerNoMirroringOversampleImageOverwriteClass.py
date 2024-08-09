a9a515bbd34b6d76b5d07888a43ff08dbc1e9833
murong <allisonmrmr@outlook.com>
2024-08-09 09:07:17 +0000

    Add training scripts of 4 customized nnUNet_v2 trainers

---
+++ train/train_nnUNetTrainerNoMirroringOversampleImageOverwriteClass.py
@@ -0,0 +1,34 @@
+from nnunetv2.run.run_training import run_training_entry
+import argparse
+import sys
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Process some inputs.')
+    parser.add_argument('dataset_name_or_id', type=str,
+                        help="Dataset name or ID to train with")
+    parser.add_argument('configuration', type=str,
+                        help="Configuration that should be trained")
+    parser.add_argument('fold', type=str,
+                        help='Fold of the 5-fold cross-validation. Should be an int between 0 and 4.')
+    parser.add_argument('-tr', type=str, required=False, default='nnUNetTrainer',
+                        help='[OPTIONAL] Use this flag to specify a custom trainer. Default: nnUNetTrainer')
+    parser.add_argument('-p', type=str, required=False, default='nnUNetPlans',
+                        help='[OPTIONAL] Use this flag to specify a custom plans identifier. Default: nnUNetPlans')
+    parser.add_argument('-prob_minority', type=float, default=None, required=False)
+    args = parser.parse_args()
+    return args
+
+def main():
+    sys.argv = ['/home/murong/22k/OMASeg_sync/OMASeg/train/train.py',
+                '553', 
+                '3d_fullres', 
+                'all',
+                '-tr', 'nnUNetTrainerNoMirroringOversampleImageOverwriteClass',
+                '-p', 'nnUNetResEncUNetLPlans',
+                '-prob_minority', '0.35'
+                ]
+    run_training_entry()
+    print('a')
+
+if __name__ == "__main__":
+    main()
