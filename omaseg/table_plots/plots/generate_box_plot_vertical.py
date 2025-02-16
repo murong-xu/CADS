7af62e830a5c26835b5bbbe5ff59a0d744558fde
murong <allisonmrmr@outlook.com>
2025-02-16 22:55:28 +0000

    Add script for vertical box-plots

---
+++ omaseg/table_plots/plots/generate_box_plot_vertical.py
@@ -0,0 +1,205 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+
+from omaseg.table_plots.utils.utils import filter_rows, align_and_filter_scores, list_specific_files, transitional_ids, amos_uterus_ids
+from omaseg.dataset_utils.bodyparts_labelmaps import labelmap_all_structure, labelmap_all_structure_renamed, structure_to_in_dist_training_dataset
+
+
+def generate_box_plot(results_dict, metric_name='Dice', output_path=None, title="Structure-wise Dice Score Distribution"):
+    """
+    Generate a vertical set of box-plots for all structures.
+    """
+    data_list = []
+    for organ, scores in results_dict.items():
+        n_samples = len(scores)
+        for score in scores:
+            data_list.append({
+                'Organ': f"{organ} (n={n_samples})",
+                metric_name: score
+            })
+    
+    df = pd.DataFrame(data_list)
+    
+    # calculate each structure's median for sorting
+    median_scores = df.groupby('Organ')[metric_name].median()
+    organ_order = median_scores.sort_values(ascending=False).index
+    
+    plt.figure(figsize=(12, len(results_dict) * 0.3))
+    
+    ax = sns.boxplot(data=df, 
+                    y='Organ', 
+                    x=metric_name,
+                    order=organ_order,
+                    color='lightcoral',
+                    whis=1.5,
+                    showfliers=True,
+                    flierprops={'marker': 'o',
+                               'markerfacecolor': 'black',  
+                               'markeredgecolor': 'black',
+                               'markersize': 2,  # same setting as stripplot
+                               'alpha': 0.3})
+    # add scatters (original data)
+    sns.stripplot(data=df,
+                 y='Organ',
+                 x=metric_name,
+                 order=organ_order,
+                 color='black',
+                 size=2,
+                 alpha=0.3,
+                 jitter=0.2)
+    
+    ax.set_xlabel(metric_name, fontsize=16)
+    ax.set_ylabel('167 Whole-body Structures', fontsize=16)
+    plt.title(title, pad=20, fontsize=20, fontweight='bold')
+    ax.grid(True, axis='x', linestyle='--', alpha=0.7)
+    ax.set_xlim(0, 1)
+    
+    if output_path:
+        plt.savefig(output_path, bbox_inches='tight', dpi=300)
+    
+    return ax
+
+if __name__ == "__main__":
+    plot_dist = 'all'  # TODO:
+    plot_metric_name = 'Dice'  # TODO:
+    plot_output_path='/mnt/hdda/murong/22k/plots/per-structure/box_plot_all_dice.png'  # TODO:
+    plot_title='Structure-wise Dice Score Distribution'  # TODO:
+
+    grouping_in_out_dist = 'group_by_omaseg_inout'  # 'group_by_omaseg_inout'/'group_by_totalseg_dataset'
+    # analysis_name = 'scores_final'
+    analysis_name = 'filtered_unreliable_and_limited_fov'
+    # analysis_name = 'filtered_unreliable'
+    # analysis_name = 'original_GT_but_remove_limited_fov'
+
+    # experiment_results_path = {
+    #     'omaseg': '/mnt/hdda/murong/22k/ct_predictions/final_models/scores_final/test_0',
+    #     'totalsegmentator': '/mnt/hdda/murong/22k/ct_predictions/baselines/totalseg/metrics_roirobust_new/test_0',
+    # }
+    experiment_results_path = {
+        'omaseg': '/mnt/hdda/murong/22k/ct_predictions/final_models/scores_labelata_confirmed_reliable_GT/test_0',
+        'totalsegmentator': '/mnt/hdda/murong/22k/ct_predictions/baselines/totalseg/metrics_labelata_confirmed_reliable_GT/test_0',
+    }
+    # experiment_results_path = {
+    #     'omaseg': '/mnt/hdda/murong/22k/ct_predictions/final_models/scores_labelata_confirmed_reliable_GT_notdo_FOV/test_0',
+    #     'totalsegmentator': '/mnt/hdda/murong/22k/ct_predictions/baselines/totalseg/metrics_labelata_confirmed_reliable_GT_notdo_FOV/test_0',
+    # }
+    # experiment_results_path = {
+    #     'omaseg': '/mnt/hdda/murong/22k/ct_predictions/final_models/scores_remove_limited_fov/test_0',
+    #     'totalsegmentator': '/mnt/hdda/murong/22k/ct_predictions/baselines/totalseg/metrics_remove_limited_fov/test_0',
+    # }
+
+    prefix = 'dice'  # TODO:
+    distributions = ['in', 'out', 'all']
+    splits = ['test']
+    filter_transitional_in_verse = True
+    exclude_face_from_overall_score = True
+
+    experiment_to_name_dict = {
+        'omaseg': 'OMASeg',
+        'totalsegmentator': 'TotalSeg',
+    }
+
+    labelmap_all_structure_inv = {v: k for k, v in labelmap_all_structure.items()}
+    table_names = list(labelmap_all_structure_renamed.values())
+    structuresids = list(labelmap_all_structure.values())
+
+    if len(table_names) != len(structuresids):
+        print("Error, the number of names and structures ids is not the same")
+        exit()
+
+    experiments_dicts = {}
+    for experiment in experiment_to_name_dict.keys():
+        structure_values = {distribution: {table_name: []
+                                        for table_name in table_names} for distribution in distributions}
+        excelfiles = list_specific_files(experiment_results_path[experiment], prefix=prefix, suffix='.xlsx')
+        for file in excelfiles:
+            base = os.path.basename(file)
+            df = pd.read_excel(file)
+            if df.shape[0] <= 1:
+                continue
+            if '0010_verse' in base and filter_transitional_in_verse:
+                df = df[~df["ids"].isin(transitional_ids)]
+            df = filter_rows(df, splits=splits)
+            column_names = df.columns
+
+            for j, s in enumerate(structuresids):
+                matching_columns = [
+                    column for column in column_names if s == column]
+                if len(matching_columns) > 0:
+                    datasetname = file.split('/')[-2]
+                    if grouping_in_out_dist == 'group_by_omaseg_inout':
+                        # decide the current structure belongs to its training group or not 
+                        structure_id = str([k for k, v in labelmap_all_structure.items() if v == s][0])
+                        structure_is_in_dist = (datasetname == structure_to_in_dist_training_dataset[structure_id])
+                    elif grouping_in_out_dist == 'group_by_totalseg_dataset':
+                        # decide the current structure belongs to totalseg dataset or not 
+                        if '0037_totalsegmentator' == datasetname:
+                            structure_is_in_dist = True
+                        else:
+                            structure_is_in_dist = False
+                    else:
+                        print("Error, select one of the following: group_by_omaseg_inout / group_by_totalseg_dataset")
+                        exit()
+
+                    for column in matching_columns:
+                        values = df[column].to_list()  # Drop NA later
+                        if datasetname == '0038_amos' and column == 'prostate':
+                            df_tmp = pd.read_excel(file)
+                            df_tmp = df_tmp[~df_tmp["ids"].isin(amos_uterus_ids)]
+                            df_tmp = filter_rows(df_tmp, splits=splits)
+                            values = df_tmp[column].to_list()
+                        if structure_is_in_dist and 'in' in structure_values:
+                            structure_values['in'][table_names[j]].extend(
+                                values)
+                        elif not structure_is_in_dist and 'out' in structure_values:
+                            structure_values['out'][table_names[j]].extend(
+                                values)
+                        if 'all' in structure_values:
+                            structure_values['all'][table_names[j]].extend(
+                                values)
+        experiments_dicts[experiment_to_name_dict[experiment]
+                        ] = structure_values
+
+    # Align scores
+    removed_data_points = {distribution: {
+        structure: 0 for structure in table_names} for distribution in distributions}
+    valid_test_data_points = {distribution: {
+        structure: 0 for structure in table_names} for distribution in distributions}
+    for distribution in distributions:
+        for structure in table_names:
+            omaseg_scores = experiments_dicts['OMASeg'][distribution][structure]
+            scores_1 = np.array(omaseg_scores)
+            scores_1 = scores_1[~np.isnan(scores_1)]
+            experiments_dicts['OMASeg'][distribution][structure] = scores_1
+
+    # # Align scores: considering baseline
+    # removed_data_points = {distribution: {
+    #     structure: 0 for structure in table_names} for distribution in distributions}
+    # valid_test_data_points = {distribution: {
+    #     structure: 0 for structure in table_names} for distribution in distributions}
+    # for distribution in distributions:
+    #     for structure in table_names:
+    #         omaseg_scores = experiments_dicts['OMASeg'][distribution][structure]
+    #         totalseg_scores = experiments_dicts['TotalSeg'][distribution][structure]
+
+    #         original_length = len(omaseg_scores)
+    #         aligned_omaseg, aligned_totalseg = align_and_filter_scores(
+    #             omaseg_scores, totalseg_scores)
+    #         new_length = len(aligned_omaseg)
+
+    #         removed_points = original_length - new_length
+    #         removed_data_points[distribution][structure] += removed_points
+
+    #         valid_test_data_points[distribution][structure] = new_length
+
+    #         experiments_dicts['OMASeg'][distribution][structure] = aligned_omaseg
+    #         experiments_dicts['TotalSeg'][distribution][structure] = aligned_totalseg
+
+
+    generate_box_plot(experiments_dicts['OMASeg'][plot_dist], 
+                      metric_name=plot_metric_name, 
+                      output_path=plot_output_path,
+                      title=plot_title)
