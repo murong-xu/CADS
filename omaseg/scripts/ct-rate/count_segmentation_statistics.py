f6d24f904f41aa625e4c4677d49ebc9ec4912414
murong <allisonmrmr@outlook.com>
2025-03-25 16:34:14 +0000

    Add statistics count

---
+++ omaseg/scripts/ct-rate/count_segmentation_statistics.py
@@ -0,0 +1,62 @@
+import glob
+import argparse
+import os
+import numpy as np
+import nibabel as nib
+import pandas as pd
+from tqdm import tqdm
+from omaseg.dataset_utils.bodyparts_labelmaps import map_taskid_to_labelmaps
+
+def process_single_case(subfolder, part, labelmap):
+    img_id = os.path.basename(subfolder)
+    segfile = os.path.join(subfolder, f'{img_id}_part_{part}.nii.gz')
+    
+    n_classes = max(labelmap.keys())
+    labels_info = [None] * n_classes
+    
+    if os.path.exists(segfile):
+        seg = nib.load(segfile).get_fdata()
+        unique = np.unique(seg)
+        
+        # Count non-background labels (skip 0)
+        if len(unique) > 1:
+            for u in unique[1:]:
+                label_idx = int(u)
+                labels_info[label_idx-1] = True
+    
+    return [img_id] + labels_info
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-part", '--part', type=int, required=True)
+    args = parser.parse_args()
+
+    part = args.part
+    predfolder = '/mnt/hdda/murong/debug_ctrate/train'  #TODO:
+    outputfolder = "/mnt/hdda/murong/debug_ctrate"  #TODO:
+    output_filename = f'debug_summary_{part}'  #TODO:
+
+    labelmap = map_taskid_to_labelmaps[part]
+    
+    columns = ['patientid']
+    names = [labelmap[i] for i in range(1, max(labelmap.keys()) + 1)]
+    columns.extend(names)
+
+    pattern = os.path.join(predfolder, '*/*_*/')
+    unique_folders = sorted(glob.glob(pattern))
+    info = []
+    
+    print(f"Processing {len(unique_folders)} cases...")
+    for folder in tqdm(unique_folders):
+        subfolder = sorted(glob.glob(folder + '*'))[0]
+        case_info = process_single_case(subfolder, part, labelmap)
+        info.append(case_info)
+
+    info = np.asarray(info)
+    df = pd.DataFrame(info, columns=columns)
+    
+    df.to_excel(os.path.join(outputfolder, output_filename + '.xlsx'), index=False)
+    np.savez(os.path.join(outputfolder, output_filename + '.npz'), info=info)
+
+if __name__ == '__main__':
+    main()
